54c8916c70806e5164e2cd88a663f01f0d13af46
Flavio Corpa <flaviocorpa@icloud.com>
2014-12-26 11:30:16 +0100

    Initial Commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Flavio Corpa on 26/12/14.
+//  Copyright (c) 2014 Flavio Corpa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
