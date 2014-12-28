a7d3ede21c0fc1d8f9633d6cb6711c5f81c37acd
kutyel <flaviocorpa@gmail.com>
2014-12-28 18:21:28 +0100

    Fix reload notes bug

--- NoteApp/NotesTableViewController.swift
+++ NoteApp/NotesTableViewController.swift
@@ -80,9 +80,9 @@ class NotesTableViewController: UITableViewController, AddNoteViewControllerDele
         
         dismissViewControllerAnimated(true, completion: nil)
         
-        notes.append(noteText)
+        //notes.append(noteText)
         
-        tableView.reloadData()
+        //tableView.reloadData()
         
         let newNote = [ "name" : noteText, "author" : "@kutyel" ]
         
@@ -103,6 +103,7 @@ class NotesTableViewController: UITableViewController, AddNoteViewControllerDele
                 println(response)
             })
             addNoteToAPI.resume()
+            loadNotes()
         }
     }
     
